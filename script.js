f470b91801fae1f7e8e194b90701c3a412ee44f3
Denia Casimiro <43858697+DeniaC5657@users.noreply.github.com>
2019-10-07 10:35:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Denia Casimiro"
+let userName = "DeniaC5657";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
